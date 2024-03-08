ef7220a412ddc5ec8d5bcbb91b2805b32e689209
jwsharpe <jameswrightsharpe@gmail.com>
2024-03-08 14:24:17 -0500

    whoom

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
 const audio = new Audio("./whitemonster.ogg");
 audio.loop = true;
-let playing = false;
+audio.currentTime = 0.5;
+
 const button = document.querySelector(".audio-container");
 const mute = document.querySelector(".mute");
 const unmute = document.querySelector(".unmute");
+
 button.addEventListener("click", () => {
-  if (playing) {
+  if (!audio.paused) {
     audio.pause();
+    audio.currentTime = 0.5;
     unmute.style.display = "none";
     mute.style.display = "";
-    playing = false;
   } else {
     audio.play();
     mute.style.display = "none";
     unmute.style.display = "";
-    playing = true;
   }
 });
